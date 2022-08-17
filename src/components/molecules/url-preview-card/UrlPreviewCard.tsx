43d1b967df5d09e4c75b104eeaa62d94238cd733
gitgitWi <gitgitWi@kakao.com>
2022-08-17 15:03:09 +0000

    feat (UrlPreviewCard) 스타일링

--- src/components/molecules/url-preview-card/UrlPreviewCard.tsx
+++ src/components/molecules/url-preview-card/UrlPreviewCard.tsx
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
+import styles from './styles.module.scss';
+
 const urlPreviewCardProps = z.object({
   title: z.string(),
+  url: z.string(),
   author: z.string().optional(),
   image: z.string().optional(),
   description: z.string().optional(),
@@ -10,14 +13,21 @@ const urlPreviewCardProps = z.object({
 export type UrlPreviewCardProps = z.infer<typeof urlPreviewCardProps>;
 
 export const UrlPreviewCard = (props: UrlPreviewCardProps) => {
-  const { title, author, image, description } = urlPreviewCardProps.parse(props);
+  const { url, title, author, image, description } = urlPreviewCardProps.parse(props);
   return (
-    <article>
-      UrlPreviewCard
-      <p>{title}</p>
-      {author && <p>{author}</p>}
-      {image && <img src={image} alt={title} width="150px" height="150px" />}
-      {description && <p>{description}</p>}
+    <article className={styles.card}>
+      {image && (
+        <div className={styles.cardLeft}>
+          <img src={image} alt={title} className={styles.cardLeftImage} />
+        </div>
+      )}
+      <div className={styles.cardRight}>
+        <a href={url} target="_blank" rel="noreferrer" className={styles.cardRightTitle}>
+          {title}
+        </a>
+        {author && <p className={styles.cardRightAuthor}>{author}</p>}
+        {description && <p className={styles.cardRightDescription}>{description}</p>}
+      </div>
     </article>
   );
 };
